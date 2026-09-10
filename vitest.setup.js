95293036fec4dad8d151d0230d03275b6f135fc8
Kouakam Henry <kmsohenry@gmail.com>
2026-09-10 22:40:08 +0100

    fix(test): keep jsdom setup compatible with Vitest 5

--- vitest.setup.js
+++ vitest.setup.js
@@ -7,16 +7,27 @@ const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>', {
   resources: 'usable',
 });
 
-global.window = dom.window;
-global.document = dom.window.document;
-global.navigator = dom.window.navigator;
-global.HTMLElement = dom.window.HTMLElement;
-global.SVGElement = dom.window.SVGElement;
-global.Element = dom.window.Element;
-global.Node = dom.window.Node;
-global.Event = dom.window.Event;
-global.CustomEvent = dom.window.CustomEvent;
-global.localStorage = {
+const globals = {
+  window: dom.window,
+  document: dom.window.document,
+  navigator: dom.window.navigator,
+  HTMLElement: dom.window.HTMLElement,
+  SVGElement: dom.window.SVGElement,
+  Element: dom.window.Element,
+  Node: dom.window.Node,
+  Event: dom.window.Event,
+  CustomEvent: dom.window.CustomEvent,
+};
+
+for (const [name, value] of Object.entries(globals)) {
+  Object.defineProperty(globalThis, name, {
+    configurable: true,
+    writable: true,
+    value,
+  });
+}
+
+globalThis.localStorage = {
   store: {},
   getItem(key) {
     return this.store[key] || null;
